510b76804628c30608b1489ae2fb4ab707ef7624
bwesterg <benton.westergaard@gmail.com>
2024-03-09 13:52:40 -0700

    adds express-oauth2-jwt-bearer middleware, env variables, and quickstart code from auth0 documentation.

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 //if a request comes to backend that says /api/my/user, then this handler is called...
-router.post("/", MyUserController.createCurrentUser)
+router.post("/", jwtCheck, MyUserController.createCurrentUser)
 //...which passes the request on to MyUserController, which handles the business logic,
 //and passed to the createCurrentUser function
 
