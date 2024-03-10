e5ce8f37b52ca46ab78af4b9d5cbe3edaeb2d693
bwesterg <benton.westergaard@gmail.com>
2024-03-10 01:11:45 -0700

    saves progres

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 //if a request comes to backend that says /api/my/user, then this handler is called...
-router.post("/", jwtCheck, MyUserController.createCurrentUser)
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 //...which passes the request on to MyUserController, which handles the business logic,
 //and passed to the createCurrentUser function
+router.put(
+  "/", 
+  jwtCheck, 
+  jwtParse, 
+  validateMyUserRequest, 
+  MyUserController.updateCurrentUser
+);
 
 export default router;
