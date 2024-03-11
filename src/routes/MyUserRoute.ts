797bdf0c535f5e120ae603406e76a2487eede4d1
bwesterg <benton.westergaard@gmail.com>
2024-03-11 14:15:00 -0600

    adds backend routes to get current user functionality and prepopulates frontend form fields with existing data. Changes are saved to the MongoDB database correctly.

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -5,6 +5,9 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
+// /api/my/user
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+
 //if a request comes to backend that says /api/my/user, then this handler is called...
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 //...which passes the request on to MyUserController, which handles the business logic,
